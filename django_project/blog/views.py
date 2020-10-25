625060440da06f50cef48bf550ebe96cf60faaee
jiklas-nust <justniklas1@gmail.com>
2020-10-25 02:05:42 +0200

    Python Django Tutorial Part 3

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,27 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'Niklas Just',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 25, 2020'
+    },
+    {
+        'author': 'Nils Just',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 25, 2020'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
